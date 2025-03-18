8a7d0c64474c5d58308e3056bdec71997facfab0
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-03-18 15:52:40 +0530

    backend users list added

--- Backend/backend_api_pjt/admin_app/urls.py
+++ Backend/backend_api_pjt/admin_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     # path('/',),
     path('register-student/', RegisterStudentAPIView.as_view(), name='register-student'),
     path('api/login/', LoginView.as_view(), name='login'),
+    path('api/students/', ListStudentsAPIView.as_view(), name='list-students'),
+
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- Backend/backend_api_pjt/admin_app/views.py
+++ Backend/backend_api_pjt/admin_app/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny,IsAdminUser
 from .serializers import StudentsSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
+from .models import *
 # Create your views here.
 
 
@@ -34,17 +35,44 @@ class LoginView(APIView):
     def post(self, request, *args, **kwargs):
         username = request.data.get('username')
         password = request.data.get('password')
+        print(username)
+        print(password,'pass')
 
         user = authenticate(username=username, password=password)
+        role = "superadmin" if user.is_superuser else "user"
         if user:
             refresh = RefreshToken.for_user(user)
             return Response({
                 'refresh': str(refresh),
                 'access': str(refresh.access_token),
+                'role': role
             }, status=status.HTTP_200_OK)
         else:
+            print('else is working')
             return Response(
                 {"error": "Invalid username or password"},
                 status=status.HTTP_401_UNAUTHORIZED
             )
+
+
+
+
+class ListStudentsAPIView(APIView):
+    permission_classes = [IsAdminUser]   
+
+    def get(self, request, *args, **kwargs):
+        print('get is working')
+        students = Students.objects.all()
+        serializer = StudentsSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
 
