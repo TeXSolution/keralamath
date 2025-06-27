46df5d415c0c945e847808e25c894c53643447b7
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-06-27 22:52:04 +0530

    updated

--- Frontend/client-app/src/pages/AdminPages/SubjectForm.jsx
+++ Frontend/client-app/src/pages/AdminPages/SubjectForm.jsx
@@ -64,8 +64,6 @@ const SubjectForm = () => {
             <div className="bg-gray-800 rounded-2xl shadow-xl p-6 w-full max-w-md">
                 <h2 className="text-3xl font-semibold mb-6 text-center">Add Subject</h2>
 
-
-
                 {/* Syllabus Dropdown */}
 
                 <div className="mb-4">
