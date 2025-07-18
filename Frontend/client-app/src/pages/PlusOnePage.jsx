ebda5e8da9eda91516eb332d234daee3ec1b2499
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-07-18 23:29:42 +0530

    frontend code command added

--- Frontend/client-app/src/pages/PlusOnePage.jsx
+++ Frontend/client-app/src/pages/PlusOnePage.jsx
@@ -13,6 +13,7 @@ const PlusOnePage = () => {
             <div className="container mx-auto px-6 py-20">
                 <h2 className="text-4xl font-bold mb-8 text-center">Plus One Class Resources</h2>
 
+                {/* BUTTON TOGGLE */}
                 <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
                     <div className="flex justify-between items-center cursor-pointer" onClick={toggleDropdown}>
                         <h3 className="text-2xl font-semibold">Resources</h3>
