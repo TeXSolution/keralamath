212483b0e0364bbcab1a71e9100be1d2fcbf14c6
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-07-20 00:20:54 +0530

    evnt added

--- Frontend/client-app/src/pages/Signup.jsx
+++ Frontend/client-app/src/pages/Signup.jsx
@@ -62,7 +62,7 @@ const Signup = () => {
             </Link>
           </p>
         </div>
-        <form className="mt-8 space-y-6" >
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <div className="space-y-4">
             {/* Full name */}
             <div>
