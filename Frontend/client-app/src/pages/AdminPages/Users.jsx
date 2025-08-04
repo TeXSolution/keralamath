ba0d147adfc9968eafbdd996f554242963afdf0c
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-04 22:28:39 +0530

    frontend try block added

--- Frontend/client-app/src/pages/AdminPages/Users.jsx
+++ Frontend/client-app/src/pages/AdminPages/Users.jsx
@@ -22,7 +22,18 @@ const Users = () => {
   // Handle Block/Unblock Action
   const handleStatusChange = async (userId, currentStatus) => {
     const newStatus = currentStatus === 'block' ? 'unblock' : 'block';
-    
+    try {
+      // Call the backend API to update the status (if needed)
+      await axiosInstance.patch(`students/${userId}/`, { status: newStatus });
+
+      setUsers((prevUsers) =>
+        prevUsers.map((user) =>
+          user.id === userId ? { ...user, status: newStatus } : user
+        )
+      );
+    } catch (error) {
+      console.error(`Error ${newStatus}ing user:`, error);
+    }
   };
 
   
