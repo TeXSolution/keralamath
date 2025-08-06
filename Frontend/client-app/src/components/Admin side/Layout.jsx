e6b93352e42d48d1621c6403a0d2d6d413a18755
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-06 22:16:59 +0530

    frontend code updated

--- Frontend/client-app/src/components/Admin side/Layout.jsx
+++ Frontend/client-app/src/components/Admin side/Layout.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
-
-// const Layout = () => {
-//   return (<>
-//     <div className="flex">
-//       <Sidebar />
-//       <main className="flex-1 p-8 bg-gray-100 min-h-screen md:ml-64">
-//         <Outlet />
-//       </main>
-//     </div>
-//   </>
-//   );
-// };
+ 
 const Layout = () => {
   return (
     <div className="flex min-h-screen">
