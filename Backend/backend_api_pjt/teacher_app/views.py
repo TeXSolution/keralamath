ab848375d8959a5d11336595e284dd725d80dd21
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-07 22:37:13 +0530

    backend import issue fixed

--- Backend/backend_api_pjt/teacher_app/views.py
+++ Backend/backend_api_pjt/teacher_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from admin_app.models import *
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import *
 # Create your views here.
 
 
