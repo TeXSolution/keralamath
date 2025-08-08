bd997ef42200ba992864466fd01c00d2c7c1dd2f
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-08 23:34:43 +0530

    backend subject listing view created

--- Backend/backend_api_pjt/teacher_app/views.py
+++ Backend/backend_api_pjt/teacher_app/views.py
@@ -4,6 +4,8 @@ from admin_app.models import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
+from admin_app.serializers import *
+from rest_framework import status
 # Create your views here.
 
 # class list view
@@ -13,3 +15,13 @@ class ClassLevelListView(APIView):
         serializer = ClassLevelSerializer(class_levels, many=True)
         return Response(serializer.data)
     
+
+
+class SubjectListAPIView(APIView):
+    def get(self, request, class_level_id=None):
+        if class_level_id:
+            subjects = Subject.objects.filter(class_level_id=class_level_id)
+        else:
+            subjects = Subject.objects.all()
+        serializer = SubjectSerializer(subjects, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
