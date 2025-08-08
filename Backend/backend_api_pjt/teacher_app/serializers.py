ad561dff3b2418c7960e9bd6eb31bd13e026d10a
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-08 23:35:30 +0530

    backend serialiser added

--- Backend/backend_api_pjt/teacher_app/serializers.py
+++ Backend/backend_api_pjt/teacher_app/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from admin_app.models import ClassLevel
+from admin_app.models import *
 
 class ClassLevelSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassLevel
         fields = '__all__'
+
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ['id', 'class_level', 'name', 'description']
