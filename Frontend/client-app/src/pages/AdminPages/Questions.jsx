22d0615b4e567d67af6a59251787423d65263425
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-12 21:44:39 +0530

    frontend bug fixed

--- Frontend/client-app/src/pages/AdminPages/Questions.jsx
+++ Frontend/client-app/src/pages/AdminPages/Questions.jsx
@@ -27,7 +27,7 @@ const Questions = () => {
     <div>
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-3xl font-bold">Questions Management</h1>
-        <button className= "">
+        <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 transition-colors">
           Add Question
         </button>
       </div>
