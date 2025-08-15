357189c9953360c87b333486fee97d069483e8c1
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-15 22:35:48 +0530

    backend url updated

--- Backend/backend_api_pjt/teacher_app/urls.py
+++ Backend/backend_api_pjt/teacher_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('api/subjects/listing/', SubjectListAPIView.as_view(), name='plus-one-subjects'),
+    path('api/subjects/listing/', SubjectListAPIView.as_view(), name='one'),
 
 
 ]
