9f31032a7e3bafc99c27ec675ba8868702ede67c
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-09-02 21:09:54 +0530

    frontend users component bug fixed

--- Frontend/client-app/src/pages/AdminPages/Users.jsx
+++ Frontend/client-app/src/pages/AdminPages/Users.jsx
@@ -6,14 +6,7 @@ const Users = () => {
 
   // Fetch users from the backend
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axiosInstance.get('students/');
-        setUsers(response.data);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
-    };
+    
 
     fetchUsers();
   }, []);
