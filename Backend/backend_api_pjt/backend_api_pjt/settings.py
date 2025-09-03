887fa9f09762b74335fcdb4606df76dac542a5fc
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-09-03 19:52:20 +0530

    backend settings command cleared

--- Backend/backend_api_pjt/backend_api_pjt/settings.py
+++ Backend/backend_api_pjt/backend_api_pjt/settings.py
@@ -5,10 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+ 
 SECRET_KEY = 'django-insecure-63iq^ms7sd_iguv&wht(u*+gcx7$01pnqytk$e8=lq^@dr%^e3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
