ecd36d82d8d11f631684d4afa63a52e2a9bc7e2c
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-10-06 20:54:37 +0530

    code command added

--- Backend/backend_api_pjt/teacher_app/urls.py
+++ Backend/backend_api_pjt/teacher_app/urls.py
@@ -4,6 +4,8 @@ from .views import *
 urlpatterns = [
     path('api/subjects/listing/', SubjectListAPIView.as_view(), name='one-view'),
 
+
+
     # CHAPTER LIST URL 
     path('chapters/listing', ChapterListAPIView.as_view(), name='chapter-list'),  
 
