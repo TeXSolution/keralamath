9c7fc7590f82c5b60ee366fd70d290f844a11ee4
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-10-06 21:00:58 +0530

    user app urls code command added

--- Backend/backend_api_pjt/user_app/urls.py
+++ Backend/backend_api_pjt/user_app/urls.py
@@ -2,10 +2,13 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
+    # SUBJECT LISTING
     path('api/subjects/listing/', SubjectListByClassAndSyllabus.as_view(), name='plus-one-subjects'),
 
+    # QUESTION LISTING
     path('api/questions/user/', QuestionListByChapter.as_view(), name='question-list'),
 
+    # CHAPTER LISTING
     path('api/chapters/user', ChapterListBySubject.as_view(), name='chapter-list-by-subject'),
 
 
