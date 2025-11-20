ee904cb63c9e7683b46357b559398ec066c9c09f
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-11-20 21:11:31 +0530

    refactor : code clean

--- Frontend/client-app/src/pages/AdminPages/Users.jsx
+++ Frontend/client-app/src/pages/AdminPages/Users.jsx
@@ -8,15 +8,7 @@ const Users = () => {
     const fetchUsers = async () => {
       try {
         const response = await axiosInstance.get('students/');
-        
-
-
-
-
-
-
-
-
+        setUsers(response.data);
         
       } catch (error) {
         console.error('Error fetching users:', error);
