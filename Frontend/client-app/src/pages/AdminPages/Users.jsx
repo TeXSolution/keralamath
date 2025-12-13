2a1656a497ec86223f225a675719c305c3cefd2f
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-12-13 16:06:42 +0530

    test : axios state

--- Frontend/client-app/src/pages/AdminPages/Users.jsx
+++ Frontend/client-app/src/pages/AdminPages/Users.jsx
@@ -7,8 +7,7 @@ const Users = () => {
     useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axiosInstance.get('students/');
-        setUsers(response.data);
+        
         
       } catch (error) {
         console.error('Error fetching users:', error);
